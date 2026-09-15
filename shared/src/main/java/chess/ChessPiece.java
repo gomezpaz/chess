1f436cf6809a3b4c5fb463f6c0de6688092f6add
Santiago Gomez Paz <santigomezpaz@gmail.com>
2026-09-15 16:20:52 -0700

    [p0] added rook moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -166,6 +166,16 @@ public class ChessPiece {
 
                 addAllMovesInVectors(board, myPosition, row, col, myTeamColor, moves, moveVectors);
             }
+            case ROOK -> {
+                int[][] moveVectors = {
+                        {+1, +0}, // up
+                        {-1, +0}, // down
+                        {+0, +1}, // right
+                        {+0, -1}  // left
+                };
+
+                addAllMovesInVectors(board, myPosition, row, col, myTeamColor, moves, moveVectors);
+            }
         }
 
         return moves;
